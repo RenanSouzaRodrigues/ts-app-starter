3581184e9fbbd737f183e3132fed3b98683b4120
Renan Souza <renan.souza@powerpush.com.br>
2022-08-12 00:00:56 -0300

    now we have some process messages inside the setup process

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 function delete_bootstrap_files {
+    echo "Removing bootstrap files from the application"
     rm -rf .git
     git init
     rm -rf ./scaffold
     rm -f LICENSE
+    echo "Done!"
 }
 
 function generate_express_application {
     echo "Generating folder from scaffold"
-    cp -r ./scaffold/TypeScript/express-app ./src
+    cp -r ./scaffold/ts/express-app ./src
     echo "Done!"
 
     echo "Removing .gitkeep files"
     rm -f ./src/app/.gitkeep
     rm -f ./src/exceptions/.gitkeep
     rm -f ./src/types/.gitkeep
+    echo "Done!"
 }
 
 if [ $1 = "express" ]; then
